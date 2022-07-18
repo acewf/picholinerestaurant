e2b35f0dded7d2902f009397d2cc8a932423d5cf
Pedro Filipe Martins <pedrom@eu.square-enix.com>
2022-07-18 17:08:15 +0100

    try fix netlify build

--- .svelte-kit/output/server/nodes/0.js
+++
@@ -1,7 +0,0 @@
-import * as module from '../entries/pages/__layout.svelte.js';
-
-export { module };
-export const index = 0;
-export const entry = 'pages/__layout.svelte-1a226073.js';
-export const js = ["pages/__layout.svelte-1a226073.js","chunks/index-3519897d.js","chunks/Link-c86dc82e.js"];
-export const css = ["assets/pages/__layout.svelte-68ec7518.css"];
